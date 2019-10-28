50cc8cc0be49f720638a779875807a82c72dbc68
Ethan Audley <ethan-a@hotmail.co.uk>
2019-10-28 11:17:51 +0000

    First Commnit

---
+++ src/app/product-list/product-list.component.ts
@@ -0,0 +1,37 @@
+import { Component } from '@angular/core';
+
+import { products } from '../products';
+import { Product } from './product-list.model';
+import { ProductService } from '../Services/products.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent {
+  products: Product[];
+
+  constructor(private productservice: ProductService) {}
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.productservice.getProducts().then(products => 
+      {this.products = products});
+  }
+
+  share() {
+    window.alert('The product has been shared!');
+  }
+
+  onNotify() {
+    window.alert('You will be notified when the product goes on sale');
+  }
+}
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://angular.io/license
+*/
